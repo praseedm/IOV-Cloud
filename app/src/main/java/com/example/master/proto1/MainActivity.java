941220e75a361e2ef87c3df8685f25faeb32a1ce
praseedm <prasi.praseed@gmail.com>
2017-05-15 22:37:10 +0530

    cloud integrated

--- app/src/main/java/com/example/master/proto1/MainActivity.java
+++ app/src/main/java/com/example/master/proto1/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.master.proto1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    TextView msg;
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference(),announRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        msg = (TextView) findViewById(R.id.announ);
+        announRef = database.child("Announcement");
+        database.child("test").setValue("test");
+        announRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                msg.setText(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
