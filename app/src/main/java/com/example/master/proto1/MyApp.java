32fd78147f5336b2723a02d54e5c7f74a5c10211
praseedm <prasi.praseed@gmail.com>
2017-06-01 12:02:51 +0530

    orientaion fixed

--- app/src/main/java/com/example/master/proto1/MyApp.java
+++ app/src/main/java/com/example/master/proto1/MyApp.java
@@ -1,8 +1,11 @@
 package com.example.master.proto1;
 
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,6 +23,42 @@ public class MyApp extends Application {
         super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         pref = this.getSharedPreferences(PREF_NAME,0);
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
     }
 
     public static void setVehicleNum(String vehicleNum) {
